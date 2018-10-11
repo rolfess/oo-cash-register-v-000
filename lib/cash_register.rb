e43a75457ef7679c8a386915c09a266d264c74a6
Susan Rolfes <rolfes57069@yahoo.com>
2018-10-11 16:57:03 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,45 +10,43 @@ class CashRegister
   end
 
 
-def add_item(item, price, quantity = 1)
-  #accept item, price, optional quantity so need a default value, adds to total and remembers total
-  item_data = {}
-  item_data[:name] = item
-  item_data[:price] = price
-  item_data[:quantity] = quantity
-  @cart << item_data
-  @last_transaction = price * quantity
-  @total = @last_transaction + @total
-  #binding.pry
-end
+  def add_item(item, price, quantity = 1)
+    #accept item, price, optional quantity so need a default value, adds to total and remembers total
+    item_data = {}
+    item_data[:name] = item
+    item_data[:price] = price
+    item_data[:quantity] = quantity
+    price.class
+    @cart << item_data
+    @last_transaction = price * quantity
+    @total = @last_transaction + @total
+    #binding.pry
+  end
 
-def apply_discount
-  #success message or no discount to apply. After discount, total is...
-  #binding.pry
-  if @discount == 0
-    return "There is no discount to apply."
-  else
+  def apply_discount
+    #success message or no discount to apply. After discount, total is...
+    #binding.pry
+    if @discount == 0
+      return "There is no discount to apply."
+    else
+      @total = @discount * 0.01 * @total + @total
+      return "After the discount, the total comes to $#{@total}."
+    end # of else
+  end
 
-    @total = @discount * 0.01 * @total + @total
-    return "After the discount, the total comes to $#{@total}."
-  end # of else
-end
-
-def items
-  #returns an array of items added to the cart
-  @cart.each do |item_data|
-    #need to account for multiple quantities of items
-     #binding.pry
-     item_names << item_data[:name]
-  end # of do
-  item_names
-  #binding.pry
-end
+  def items
+    #returns an array of items added to the cart
+    @cart.each do |item_data|
+      #need to account for multiple quantities of items
+       #binding.pry
+       item_names << item_data[:name]
+    end # of do
+    item_names
+    #binding.pry
+  end
 
 def void_last_transaction
   #removes the last item from the cart and total
   @total = @total - @last_transaction
 end
-
-
 end # of class
